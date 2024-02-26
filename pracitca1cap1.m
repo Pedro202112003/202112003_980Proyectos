f4235c12fb511585604f6c71217c2cb278b63b66
Pedro202112003 <158627764+Pedro202112003@users.noreply.github.com>
2024-02-25 22:34:36 -0600

    fisrt commit

---
+++ pracitca1cap1.m
@@ -0,0 +1,39 @@
+#OBEJETOS NUMERICOS
+375
+3.75e2
+3.75E2
+0x177
+#numero_inicioal:salto:numero_final
+1:0.5:10
+#matrices
+H = [1, 2, 3;4, 5, 6;7, 8 ,9]
+M= [1:4;5:8]
+#cadenas de caracteres
+'cadenas_de_string'
+#sonido de windowa
+"\a"
+#estructuras
+x = {}
+x.secuencia = 1:5
+x.matriz = [1,2;4,5]
+x.string = 'secuencia'
+x.estructura = {}
+x.estructura.numero = 0x177
+x.estructura.letra = 'A'
+x
+#tipos de operadores
+x = 2
+y = 3
+x + y
+x * y
+x / y
+++x
+--x
+
+x < y
+x <= y
+x > y
+x >= y
+x == y
+x != y
+
